27636b3d6bf9812f48e168a96a86bdb65f219a21
Aakash Deshpande <aakash.deshpande@studiographene.com>
2023-08-17 11:52:36 +0530

    list of string added

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "aws_security_group" {
   enabled = local.create_security_group
 
   allow_all_egress    = var.allow_all_egress
-  security_group_name = coalesce(var.security_group_name, "${module.this.id}-elasticache-redis")
+  security_group_name = coalesce([var.security_group_name, "${module.this.id}-elasticache-redis"])
   rules_map = {
     extra = var.additional_security_group_rules
   }
